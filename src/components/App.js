8721aa9d342b40475d9386b636b98b1a65f76ece
Nadezhda Komaritskaya <n.komaritskaya@pulkovo-service.ru>
2019-09-17 18:22:00 +0300

    first commit

---
+++ src/components/App.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { getUsersRequest, createUserRequest, deleteUserRequest, usersError } from '../actions/users';
+import UsersList from './UsersList';
+import NewUserForm from './NewUserForm';
+import { Alert } from 'reactstrap';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.props.getUsersRequest();
+  }
+
+  handleSubmit = ({ firstName, lastName }) => {
+    this.props.createUserRequest({ firstName, lastName });
+  }
+
+  handleDeleteUserClick = (userId) => {
+    this.props.deleteUserRequest(userId);
+  }
+
+  handleCloseAlert = () => {
+    this.props.usersError({
+      error: '',
+    });
+  }
+
+  render() {
+    const users = this.props.users;
+    return (
+      <div style={{ margin: '0 auto', padding: '20px', maxWidth: '600px' }}>
+        <Alert color="danger" isOpen={!!this.props.users.error} toggle={this.handleCloseAlert}>
+          {this.props.users.error}
+        </Alert>
+        <NewUserForm onSubmit={this.handleSubmit} />
+        <UsersList users={users.items} onDeleteUser={this.handleDeleteUserClick} />
+      </div>
+    );
+  }
+}
+
+export default connect(({ users }) => ({ users }), {
+  getUsersRequest, createUserRequest, deleteUserRequest, usersError
+})(App);
